6be8a5721e9d043a82987b0b569959743fdbce34
Rakesh Goud Burra <Rakesh Goud Burra>
2023-05-09 17:59:55 +0530

    First commit

---
+++ AWSExample/src/main/java/com/awss3/sdk/AwsSdkExampleApplication.java
@@ -0,0 +1,13 @@
+package com.awss3.sdk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsSdkExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsSdkExampleApplication.class, args);
+	}
+
+}
---
+++ AWSExample/src/test/java/com/awss3/sdk/AwsSdkExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.awss3.sdk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwsSdkExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
